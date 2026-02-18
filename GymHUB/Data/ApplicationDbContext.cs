f7e721192b35e26031b277a6a86df5000620a648
Haris Sabanovic <hasa2303@student.miun.se>
2026-02-18 15:12:30 +0100

    Skapar tabeller i databasen

--- GymHUB/Data/ApplicationDbContext.cs
+++ GymHUB/Data/ApplicationDbContext.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using GymHUB.Models;
 
 namespace GymHUB.Data
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    public class ApplicationDbContext : IdentityDbContext<User>
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        //tabeller
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<InstructorRoom> InstructorRooms { get; set; }
+        public DbSet<ClassSession> ClassSessions { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
     }
 }
