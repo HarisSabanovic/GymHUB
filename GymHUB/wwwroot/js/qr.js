d233fca1f37b40ade3f9e69479ab6ef0fedcfb8e
Haris Sabanovic <hasa2303@student.miun.se>
2026-03-24 23:11:21 +0100

    skapar qr kod

---
+++ GymHUB/wwwroot/js/qr.js
@@ -0,0 +1,15 @@
+﻿window.addEventListener("load", function () {
+    const qrCodeData = document.getElementById("qrCodeData");
+    const qrCodeElement = document.getElementById("qrCode");
+
+    if (!qrCodeData || !qrCodeElement) return;
+
+    const qrCodeUri = qrCodeData.getAttribute("data-url");
+    if (!qrCodeUri) return;
+
+    new QRCode(qrCodeElement, {
+        text: qrCodeUri,
+        width: 200,
+        height: 200
+    });
+});
