84bba984c1289c16c1fd52183b5ef8125e975d1c
Haris Sabanovic <hasa2303@student.miun.se>
2026-03-24 23:12:24 +0100

    hämtar/skapae qr kod från api

---
+++ GymHUB/wwwroot/lib/qrcode.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    function QRCode(el, options) {
+        const text = options.text;
+        const size = options.width || 200;
+
+        const img = document.createElement("img");
+        img.src = "https://api.qrserver.com/v1/create-qr-code/?size=" + size + "x" + size + "&data=" + encodeURIComponent(text);
+
+        el.innerHTML = "";
+        el.appendChild(img);
+    }
+
+    window.QRCode = QRCode;
+})();
